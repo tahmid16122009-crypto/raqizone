10128a4690d7ace03da2bea8e8ca38c17f7a460f
tahmid16122009-crypto <tahmid16122009@gmail.com>
2026-06-03 23:04:54 +0600

    Update index.php

--- raqizone.com/index.php
+++ raqizone.com/index.php
@@ -1,1 +1,11 @@
-h
+<?php
+require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/auth.php';
+
+$u = get_current_user();
+if ($u) {
+    header('Location: /home');
+} else {
+    header('Location: /pages/welcome.php');
+}
+exit;
